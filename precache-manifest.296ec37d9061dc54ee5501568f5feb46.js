cf64e55b459e2db515383c30017e7e21ef5f89b4
Nitai Shavit <nitai230@gmail.com>
2020-08-18 06:46:52 +0300

    Updates

---
+++ precache-manifest.296ec37d9061dc54ee5501568f5feb46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e001545fbc77a4bba4c23a1534b0ae2",
+    "url": "/Nitai-Shavit-14-08-2020/index.html"
+  },
+  {
+    "revision": "043260999c58cdd715eb",
+    "url": "/Nitai-Shavit-14-08-2020/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "c9521f5dfe4448d0892e",
+    "url": "/Nitai-Shavit-14-08-2020/static/css/main.93cbee15.chunk.css"
+  },
+  {
+    "revision": "043260999c58cdd715eb",
+    "url": "/Nitai-Shavit-14-08-2020/static/js/2.32ce4a75.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/Nitai-Shavit-14-08-2020/static/js/2.32ce4a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9521f5dfe4448d0892e",
+    "url": "/Nitai-Shavit-14-08-2020/static/js/main.2603ebb0.chunk.js"
+  },
+  {
+    "revision": "76b2033f34f7610fd06a",
+    "url": "/Nitai-Shavit-14-08-2020/static/js/runtime-main.ca8ebea7.js"
+  }
+]);
